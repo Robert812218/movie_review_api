50fe632af2311afe9b2377a1cbfde8f7d4c823f9
Robert Kelly <robert812@shaw.ca>
2022-10-29 12:25:54 -0600

    update

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req, res) => {
+	res.send('<h1>RESPONSE FROM BACKEND SERVER</h1>');
+})
+
+app.get('/about', (req, res) => {
+	res.send("<h1>RESPONSE FROM BACKEND SERVER</h1>");
+});
+
 app.listen(8000, () => {
 	console.log("port listening on port 8000");
 })
